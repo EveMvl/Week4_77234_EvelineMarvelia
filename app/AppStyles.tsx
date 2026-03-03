cd86dfa76b2abfa6486b0ae5e223b3b74a1e9167
EveMvl <evelinemarvelia@gmail.com>
2026-03-03 23:38:40 +0700

    Asignment done

---
+++ app/AppStyles.tsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    backgroundColor: "#fff0f5"
+  },
+  card: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#ffe4ec",
+    padding: 15,
+    borderRadius: 20,
+    marginBottom: 10
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 15
+  },
+  boldText: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#c2185b"
+  }
+});
